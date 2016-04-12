1817cb2c6fa64cbcab7eefbf91d1d2c9e948774a
QiZhong Lin <qizhong.lin@philips.com>
2016-04-12 11:20:56 +0800

    init project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bootstrapExApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bootstrapExApp
+ */
+angular.module('bootstrapExApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
